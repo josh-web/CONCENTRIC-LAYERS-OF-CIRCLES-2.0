a100d0b80ac07973645bb794900582e550441c32
Joshua <55012411+josh-web@users.noreply.github.com>
2019-10-15 16:52:38 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CONCENTRIC-LAYERS-OF-CIRCLES-2.0
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(1500,400);
+}
+
+function draw() {
+  background(0);  
+  fill("purple");
+  ellipse(200,200,300,300); 
+fill("maroon");
+  ellipse(200,200,250,250);
+  fill("blue");
+  ellipse(200,200,220,220);
+  fill("green");
+  ellipse(200,200,150,150);
+  drawSprites();
+}
